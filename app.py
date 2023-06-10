87d56c922d64e1b40c03576cc6fa4c8cf923840b
rickluiz <rickluizms@gmail.com>
2023-06-10 19:15:04 -0300

    update commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, send_file, make_response, send_from_directory, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/resume', methods=['GET'])
+def resume():
+    return render_template('resume.html')
+
+@app.route('/projects', methods=['GET'])
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+# Subir em HEROKU
